ef3f7f12b51b23531e77d532c3b9c2040722631f
Henrique Izar Santos Machado <henriqueizarmachado@gmail.com>
2025-12-24 20:47:54 -0300

    fix: file search fixed by useStaticAssets on endpoint /uploads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:3001',
-    credentials: true,
-  });
+  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // No seu main.ts
+  app.useStaticAssets(join(process.cwd(), 'uploads'), {
+    prefix: '/uploads', 
+    index: false,       // nao busca index.html
+});
+  app.enableCors();
 
   await app.listen(3000);
 }
